d69a1bcab6d08a46a192a17f7f32f2d662478315
monkey <m.b.rosario@hotmail.com>
2020-06-04 07:30:20 +0100

    list queue implemented.

--- aed_ds/queues/list_queue.py
+++ aed_ds/queues/list_queue.py
@@ -1,12 +1,30 @@
 from .tad_queue import Queue
+from ..lists.singly_linked_list import SinglyLinkedList
+from ..exceptions import EmptyQueueException, FullQueueException
+
 
 class ListQueue(Queue):
-    def is_empty(self): pass
+    def __init__(self):
+        self.list = SinglyLinkedList()
 
-    def is_full(self): pass
+    def is_empty(self):
+        return self.list.size() == 0
 
-    def size(self): pass
+    def is_full(self):
+        return False
 
-    def enqueue(self, element): pass
+    def size(self):
+        return self.list.size()
 
-    def dequeue(self): pass
+    def enqueue(self, element):
+        if self.is_full():
+            raise FullQueueException()
+        else:
+            return self.list.size()
+
+    def dequeue(self):
+        if self.is_empty():
+            raise EmptyQueueException()
+        else:
+            self.list.remove_first()
+

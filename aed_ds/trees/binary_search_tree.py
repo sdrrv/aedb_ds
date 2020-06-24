ca2d485e4cfe982ffd2e14670b4b81ca8fdbd05e
monkey <m.b.rosario@hotmail.com>
2020-06-24 13:28:57 +0100

    easy stuff out of the way

--- aed_ds/trees/binary_search_tree.py
+++ aed_ds/trees/binary_search_tree.py
@@ -6,11 +6,17 @@ from .nodes.binary_nodes import BinarySearchTreeNode
 
 
 class BinarySearchTree(OrderedDictionary, Tree):
+    def __init__(self):
+        self.root = BinarySearchTreeNode()
+        self.count = 0
+
     # Returns the number of elements in the dictionary.
-    def size(self): pass
+    def size(self):
+        return self.count
 
     # Returns true if the dictionary is full.
-    def is_full(self): pass
+    def is_full(self):
+        return False
 
     # Returns the value associated with key k.
     # Throws NoSuchElementException
@@ -18,7 +24,8 @@ class BinarySearchTree(OrderedDictionary, Tree):
 
     # Inserts a new value, associated with key k.
     # Throws DuplicatedKeyException
-    def insert(self, k, v): pass
+    def insert(self, k, v):
+        self.root = self.insert_element(self.root, k, v)
 
     # Updates the value associated with key k.
     # Throws NoSuchElementException
@@ -42,20 +49,53 @@ class BinarySearchTree(OrderedDictionary, Tree):
 
     # Returns the element with the smallest key
     # Throws EmptyTreeException
-    def get_min_element(self): pass
-    
+    def get_min_element(self):
+        if self.is_empty():
+            raise EmptyDictionaryException
+        return self.get_min_node(self.root).get_element()
+
     # Returns the element with the largest key
     # Throws EmptyTreeException
-    def get_max_element(self): pass
+    def get_max_element(self):
+        if self.is_empty():
+            raise EmptyDictionaryException
+        return self.get_max_node(self.root).get_element()
 
     # Returns the root of the tree
     # Throws EmptyTreeException
-    def get_root(self): pass
+    def get_root(self):
+        return self.root.get_element()
 
     # Returns the height of the tree
     # Throws EmptyTreeException
     def height(self): pass
 
     # Returns True if the tree is empty
-    def is_empty(self): pass
-    
+    def is_empty(self):
+        return self.count == 0
+
+    def get_min_node(self, root):
+        if root.get_left_child() is None:
+            return root
+        return self.get_min_node(root.get_left_child())
+
+    def get_max_node(self, root):
+        if root.get_right_child() is None:
+            return root
+        return self.get_max_node(root.get_right_child())
+
+    def insert_element(self, root, k, v):
+        if root is None:
+            root = BinaryTreeNode(k, v)
+            self.count += 1
+        else:
+            if root.get_key() == k:
+                raise DuplicatedKeyException
+            elif root.get_key() > k:
+                node = self.insert_element(root.get_left_child(), k, v)
+                root.set_left_child(node)
+            else:
+                node = self.insert_element(root.get_right_child(), k, v)
+                root.set_right_child(node)
+        return root
+
